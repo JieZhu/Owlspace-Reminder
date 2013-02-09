cc2357a6db88dfeb48ffb0c55488cded7f8b3874
Aaron Roe <aaronalkan@gmail.com>
2013-02-09 14:55:45 -0600

    Adds Debugging for python script

--- scripts/php/data_funcs.php
+++ scripts/php/data_funcs.php
@@ -4,7 +4,15 @@
    * Function to get Owlspace Data, given Netid/Pass
    */
   function getOwlspaceData($netid, $pass) {
-    $data = system('python ' . $netid . $pass, $retval);
+    $command = 'python ../python/get_stuffs.py ' . $netid . ' ' . $pass;
+    $data = system($command, $retval);
+
+    echo $data;
+
+    return $data;
   }
 
+  echo 'starting call';
+  echo getOwlspaceData("yz34","zHOUYANSEN2006");
+  echo 'ending call';
 ?>
