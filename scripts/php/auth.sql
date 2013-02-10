1d86c2b6db08a2ea722bb6b08e64f85b926b149c
Aaron Roe <aaronalkan@gmail.com>
2013-02-10 04:41:05 -0600

    Updates sql, and hooks up preferences page

--- scripts/php/auth.sql
+++ scripts/php/auth.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS assignments (
   id int(11) NOT NULL AUTO_INCREMENT,
+  selected varchar(1) CHARACTER SET utf8 NOT NULL,
   netid varchar(10) CHARACTER SET utf8 NOT NULL,
   class varchar(20) CHARACTER SET utf8 NOT NULL,
   assignment varchar(20) CHARACTER SET utf8 NOT NULL,
--- scripts/php/navbar.php
+++ scripts/php/navbar.php
@@ -6,17 +6,17 @@
               <ul class="nav">';
     if(isset($_SESSION['netid'])) {
       if($pagename == 'assignments.php')
-        echo '<li class="active"><a href="#">Assignments</a></li>';
+        echo '<li class="active"><a href="assignments.php">Assignments</a></li>';
       else
-        echo '<li><a href="#">Assignments</a></li>';
+        echo '<li><a href="assignments.php">Assignments</a></li>';
       if($pagename == 'preferences.php')
-        echo '<li class="active"><a href="#">Preferences</a></li>';
+        echo '<li class="active"><a href="preferences.php">Preferences</a></li>';
       else
-        echo '<li><a href="#">Preferences</a></li>';    
+        echo '<li><a href="preferences.php">Preferences</a></li>';    
       echo '<li><a href="./scripts/php/logout.php">Logout</a></li>';
     }
     else {
-      echo '<li class="active"><a href="#">Login</a></li>';
+      echo '<li class="active"><a href="index.php">Login</a></li>';
     }
     echo '</ul>
           </div>
