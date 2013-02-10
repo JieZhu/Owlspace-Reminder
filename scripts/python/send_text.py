aa0176d60b3e6b2b1d50198f89074459a29c7045
root <root@yansuck.me>
2013-02-10 04:48:26 -0600

    more shit

--- scripts/python/send_text.py
+++ scripts/python/send_text.py
@@ -6,4 +6,4 @@ phone, message = sys.argv[1:3]
 
 command = "curl -X POST 'https://api.twilio.com/2010-04-01/Accounts/AC7af756065e479f2498715b47ddf0fc4a/SMS/Messages.json' -d 'From=%2B12813771843' -d 'To=%2B1"+ phone +"' -d 'Body="+message+ "' -u AC7af756065e479f2498715b47ddf0fc4a:329a19ff9099dad75522a5cdcc435e22"
 
-print commands.getstatusoutput(command)
+commands.getstatusoutput(command)
