9ff807a0d1854a0938f2e5560cff61a7f7d6d506
Aaron Roe <aaronalkan@gmail.com>
2013-02-10 01:33:27 -0600

    Changes the sql file to utf8

--- scripts/php/auth.sql
+++ scripts/php/auth.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS users (
   id int(11) NOT NULL AUTO_INCREMENT,
-  netid varchar(10) CHARACTER SET latin1 NOT NULL,
-  password varchar(64) CHARACTER SET latin1 NOT NULL,
-  salt varchar(5) CHARACTER SET latin1 NOT NULL,
+  netid varchar(10) CHARACTER SET utf8 NOT NULL,
+  password varchar(64) CHARACTER SET utf8 NOT NULL,
+  salt varchar(5) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY username (netid)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+  UNIQUE KEY (netid)
+) DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS assignments (
   id int(11) NOT NULL AUTO_INCREMENT,
-  netid varchar(10) CHARACTER SET latin1 NOT NULL,
-  class varchar(20) CHARACTER SET latin1 NOT NULL,
-  assignment varchar(20) CHARACTER SET latin1 NOT NULL,
+  netid varchar(10) CHARACTER SET utf8 NOT NULL,
+  class varchar(20) CHARACTER SET utf8 NOT NULL,
+  assignment varchar(20) CHARACTER SET utf8 NOT NULL,
   deadline int(10) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY username (netid)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+  UNIQUE KEY (netid)
+) DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
