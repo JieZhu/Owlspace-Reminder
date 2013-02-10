9e021303de3742ab55ea50e5572dd80e0eed215e
Aaron Roe <aaronalkan@gmail.com>
2013-02-10 06:53:00 -0600

    Adds commented out code

--- scripts/php/vault.php
+++ scripts/php/vault.php
@@ -1,3 +1,3 @@
 <?php
-  $serverKey = 'ygOk5xXT5GR3ouCUKm6nHLHLrEFeBbYOM058h7sh6kpAcOOoMi1UNX6ptdEn3SgwufvTi0kQroR1QkN0ZPzwkpehXqUBNazpFafuakq2vWYJ30tssHWKsG7BvKtPnRKk';
+  $serverKey = 'ufvTi0kQroR1QkN0ZPzwkpehXqUBNazpFafuakq2vWYJ30tssHWKsG7BvKtPnRKk';
 ?>
